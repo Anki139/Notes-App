1533aaab83faa10359e2bcbfcdc053d25ae5e265
Anki139 <chauhan123anki@gmail.com>
2025-07-29 15:27:24 +0530

    Update db.ts

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const pool = new Pool({
+export const db = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
